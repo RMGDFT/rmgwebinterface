c14fa1c7e8a0c05c71a12fb5047566c76197f837
WenchangLu <wlu2@ncsu.edu>
2022-06-01 15:37:45 -0400

    fix bug for view_xyzw

--- view_xyz.py
+++ view_xyz.py
@@ -5,8 +5,25 @@ from stmol import showmol
 def view_xyz(crmg):
     xyz_data ="%d\n\n"%(len(crmg.atoms))
     
-    for atom in  crmg.atoms:
-        xyz_data += "%s  %f %f %f\n"%(atom[0], atom[1] * crmg.cell.lengthscale,atom[2] * crmg.cell.lengthscale,atom[3] * crmg.cell.lengthscale)
+    if crmg.atom_unit == "Cell Relative":
+        for atom in crmg.atoms:
+            x = atom[1] * crmg.cell.latticevectors[0][0]
+            x += atom[2] * crmg.cell.latticevectors[1][0]
+            x += atom[3] * crmg.cell.latticevectors[2][0]
+            y = atom[1] * crmg.cell.latticevectors[0][1]
+            y += atom[2] * crmg.cell.latticevectors[1][1]
+            y += atom[3] * crmg.cell.latticevectors[2][1]
+            z = atom[1] * crmg.cell.latticevectors[0][2]
+            z += atom[2] * crmg.cell.latticevectors[1][2]
+            z += atom[3] * crmg.cell.latticevectors[2][2]
+            x *= crmg.cell.lengthscale
+            y *= crmg.cell.lengthscale
+            z *= crmg.cell.lengthscale
+
+            xyz_data += "%s  %f %f %f\n"%(atom[0], x,y,z)
+    else:
+        for atom in  crmg.atoms:
+            xyz_data += "%s  %f %f %f\n"%(atom[0], atom[1] ,atom[2],atom[3])
 
 
     xyz_view = py3Dmol.view()
