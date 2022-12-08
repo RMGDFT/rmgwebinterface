976c6a573529b538d770a4c7d227fe31c1a39b16
WenchangLu <wlu2@ncsu.edu>
2022-12-08 11:44:46 -0500

    fix make_supercell when "cell relative" unit for atoms.wq

--- make_supercell.py
+++ make_supercell.py
@@ -8,7 +8,8 @@ def make_supercell(crmg, cell_mult):
     crmg.cell.c = crmg.cell.c * cells[2]
     atoms_super = []
 
-    for i in range(cells[0]):
+    if crmg.atom_unit == "Absolute":
+       for i in range(cells[0]):
         for j in range(cells[1]):
             for k in range(cells[2]):
                 x = i* crmg.cell.latticevectors[0][0] + j* crmg.cell.latticevectors[1][0] + k* crmg.cell.latticevectors[2][0] 
@@ -21,6 +22,14 @@ def make_supercell(crmg, cell_mult):
                 for atom in  crmg.atoms:
                     new_atom = [atom[0], atom[1] + x, atom[2] +y, atom[3] + z]
                     atoms_super.append(new_atom)
+    else:
+       for i in range(cells[0]):
+        for j in range(cells[1]):
+            for k in range(cells[2]):
+                for atom in  crmg.atoms:
+                    new_atom = [atom[0], (atom[1] + i)/float(cells[0]), (atom[2] +j)/float(cells[1]), (atom[3] + k)/float(cells[2])]
+                    atoms_super.append(new_atom)
+
     for i in range(3):
         for j in range(3):
             crmg.cell.latticevectors[i][j] = crmg.cell.latticevectors[i][j] * cells[i] 
